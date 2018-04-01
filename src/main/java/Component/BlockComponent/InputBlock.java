679505f32a17bf4616fd653b8c10f501bc7e577b
ChaeByunghoon <cbh1203@naver.com>
2018-04-01 22:40:26 +0900

    InputBlock.java FileOpenListener 폴더도 가능하게 수정 getFileType으로 파일 타입을 검사하는 함수 추가, PreprocessorBlock에 InputBlock에서 검사된 파일타입을 받을 수 있는 함수 추가

--- src/main/java/Component/BlockComponent/InputBlock.java
+++ src/main/java/Component/BlockComponent/InputBlock.java
@@ -1,5 +1,7 @@
 package Component.BlockComponent;
 
+import Const.FileType;
+
 import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.event.ActionEvent;
@@ -11,6 +13,9 @@ public class InputBlock extends Block{
     private JButton openFileExploreButton;
     // 불러온 파일패스를 보여주는 텍스트필드 유저가 직접 편집할수 없게 설정
     private JTextField filePathTextField;
+    // csv파일이나 텍스트 파일의 경우 맨 끝의 데이터를 제거할 것인지 맨 처음의 데이터를 제거할 것인지의 옵션.
+    private JRadioButton endEliminationRadioButtion;
+    private JRadioButton startEliminationRadioButton;
 
 
     public InputBlock(){
@@ -19,6 +24,8 @@ public class InputBlock extends Block{
         filePathTextField = new JTextField();
         // 파일패스를 직접 설정하지 못하게..
         filePathTextField.setEditable(false);
+        endEliminationRadioButtion = new JRadioButton("맨 끝 제거");
+        startEliminationRadioButton = new JRadioButton("시작 제거");
 
     }
 
@@ -28,18 +35,22 @@ public class InputBlock extends Block{
         @Override
         public void actionPerformed(ActionEvent e) {
             JFileChooser jFileChooser = new JFileChooser();
+            //폴더와 디렉토리 둘다 가능
+            jFileChooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
             // TODO 확장자 정리
-            FileNameExtensionFilter filter = new FileNameExtensionFilter("JPG","jpg");
-            jFileChooser.setFileFilter(filter);
-
+            // 폴더를 선택했을 떄의 옵션도 필요하기때문에 주석처
+           // FileNameExtensionFilter filter = new FileNameExtensionFilter("JPG","jpg");
+           // jFileChooser.setFileFilter(filter);
 
             int ret = jFileChooser.showOpenDialog(null);
             if(ret != JFileChooser.APPROVE_OPTION){
                 JOptionPane.showMessageDialog(null, "파일을 선택하지 않았습니다.","경고", JOptionPane.WARNING_MESSAGE);
             }
 
+            //TODO 폴경로는 다 확보했으나 폴더를 받았을 때 파일리스트를 넘겨줄 것인가 아니면 알아서 할 것인지 논의 필요
             String filePath = jFileChooser.getSelectedFile().getPath();
             filePathTextField.setText(filePath);
+
         }
     }
     @Override
@@ -58,6 +69,14 @@ public class InputBlock extends Block{
     boolean isPreviousBlockConnectable(Block block) {
         return false;
     }
+
+    public FileType getFileType(){
+        //파일 타입을 판단하는 알고리즘 필요
+
+        return FileType.TYPE_NUMBER;
+    }
+
+
 
 
 }
--- src/main/java/Component/BlockComponent/PreprocessorBlock.java
+++ src/main/java/Component/BlockComponent/PreprocessorBlock.java
@@ -1,9 +1,17 @@
 package Component.BlockComponent;
 
+import Const.FileType;
+
+import javax.swing.*;
+
 /**
  * Created by chaebyeonghun on 2018. 3. 27..
  */
 public class PreprocessorBlock extends Block {
+
+    // 몇개 더 추가해야하는데 기억이 안나서 이것만 해놓음.
+    JRadioButton rgbOptionRadioButton;
+    FileType fileType;
 
 
     public PreprocessorBlock(){
@@ -16,8 +24,6 @@ public class PreprocessorBlock extends Block {
     }
 
 
-
-
     @Override
     boolean isNextBlockConnectable(Block block) {
         return (block instanceof LayerBlock);
@@ -25,6 +31,14 @@ public class PreprocessorBlock extends Block {
 
     @Override
     boolean isPreviousBlockConnectable(Block block) {
+        if(block instanceof InputBlock){
+            getFileType((InputBlock) block);
+        }
         return (block instanceof InputBlock);
+    }
+    // Preprocessor랑 Input블록이 연결될때 무조건 호출해야하는 함수
+    public void getFileType(InputBlock inputBlock){
+        //이전 Input 블록에서 파일타입을 가져옴
+        fileType = inputBlock.getFileType();
     }
 }
