e978cc83ab220a4c5c7fada3af71cb7c3b546938
park hae sung <hea9549@gmail.com>
2018-04-10 01:42:23 +0900

    Feat : 1.0 inference template python code finish

--- src/main/java/ML/Core/Python/TensorFlow/CodeTemplate/InferenceTemplate.py
+++ src/main/java/ML/Core/Python/TensorFlow/CodeTemplate/InferenceTemplate.py
@@ -1,7 +1,16 @@
 import codecs
-import LayerGeneartor
-import tensorflow
+from sklearn import preprocessing
+import numpy as np
+import os
+import LayerGeneartor as lg
+import PreProcessor as pp
+import FileReader as fr
+
 
 class Inferencer:
-    def __init__(self,x_file_path,y_file_path,is_x_folder=False):
-        codecs.open(y_file_path, "r", "utf-8")
+    def __init__(self,x_file_path, y_file_path, x_using_option="ALL", y_using_option="ALL"):
+        self.X , self.Y = fr.read_file(x_file_path, y_file_path, x_using_option=x_using_option, y_using_option=y_using_option)
+
+    def get_logit(self):
+        # below here, MLLego will generate source code.
+        pass
